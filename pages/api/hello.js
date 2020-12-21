a83e012c49526c3fbd4b4a5d679f21815387ae38
饶家俊 <raojiajun@bimwinner.com>
2020-12-21 16:58:46 +0800

    尝试在 next 接入 typeorm ts ,但是报错了

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,15 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import "reflect-metadata";
+import {createConnection} from "typeorm";
+import {Photo} from "../../src/entity/Photo.ts";
 
 export default (req, res) => {
+
+	createConnection().then(connection => {
+		// here you can start to work with your entities
+		console.log(connection);
+	}).catch(error => console.log(error));
+	
+	
   res.statusCode = 200
   res.json({ name: 'John Doe' })
 }
